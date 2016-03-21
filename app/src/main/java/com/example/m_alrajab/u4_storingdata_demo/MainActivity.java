2b122e279dc0dd6def5e3b1c0805dc8e7ec3f2e8
Benjamin <scalerab1@mail.montclair.edu>
2016-03-21 13:05:24 -0400

    register button

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/MainActivity.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.m_alrajab.u4_storingdata_demo;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,7 +14,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
 
-  
+        Button reg_button=(Button) findViewById(R.id.btn_register_MA);
+        reg_button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, RegistrationPage.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
