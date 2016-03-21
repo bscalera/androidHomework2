df66dc9b77f99e62aefcdcd8592d44de99158714
Benjamin <scalerab1@mail.montclair.edu>
2016-03-21 13:37:08 -0400

    password action

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/MainActivity.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.m_alrajab.u4_storingdata_demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -22,6 +25,17 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        EditText editText_password=(EditText) findViewById(R.id.editText_password_MA2);
+        editText_password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                Intent intent=new Intent(MainActivity.this, RegistrationPage.class);
+                startActivity(intent);
+                return false;
+            }
+        });
+
     }
 
 
