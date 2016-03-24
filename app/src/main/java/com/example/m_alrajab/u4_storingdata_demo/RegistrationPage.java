d46457c718668d477fabbaf43bea661f85813954
Benjamin <scalerab1@mail.montclair.edu>
2016-03-24 15:28:43 -0400

    added date of birth java code

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/RegistrationPage.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/RegistrationPage.java
@@ -1,5 +1,8 @@
 package com.example.m_alrajab.u4_storingdata_demo;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -8,7 +11,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+
+import java.util.Calendar;
 
 public class RegistrationPage extends AppCompatActivity {
     SharedPreferences sharedPreferences;
@@ -39,6 +45,36 @@ public class RegistrationPage extends AppCompatActivity {
 
                 Intent intent=new Intent(RegistrationPage.this,MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+
+        EditText reg_page_date_picker=(EditText) findViewById(R.id.Reg_dateOfBirth);
+        final int day, month, year;
+        final Context context;
+
+        reg_page_date_picker.setOnClickListener(new View.OnClickListener() {
+            //@Override
+
+
+            class DatePickerFragment extends DialogFragment
+                    implements DatePickerDialog.OnDateSetListener {
+
+                @Override
+                public Dialog onCreateDialog(Bundle savedInstanceState) {
+                    // Use the current date as the default date in the picker
+                    final Calendar c = Calendar.getInstance();
+                    int year = c.get(Calendar.YEAR);
+                    int month = c.get(Calendar.MONTH);
+                    int day = c.get(Calendar.DAY_OF_MONTH);
+
+                    // Create a new instance of DatePickerDialog and return it
+                    return new DatePickerDialog(getActivity(), this, year, month, day);
+                }
+
+                public void onDateSet(DatePicker view, int year, int month, int day) {
+                    // Do something with the date chosen by the user
+                }
             }
         });
 
