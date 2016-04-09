cb2b3d230d43a33c755dab634ecc3be014f39890
Benjamin <scalerab1@mail.montclair.edu>
2016-04-08 23:30:23 -0400

    The notes activity is shown in the event history

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/NotesActivity.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/NotesActivity.java
@@ -1,7 +1,10 @@
 package com.example.m_alrajab.u4_storingdata_demo;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Date;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class NotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        
+
+        logActivity();
+    }
+
+    public void logActivity()
+    {
+        Date currentTime = new Date();
+        String history;
+        SharedPreferences sharedPreferences = getSharedPreferences("history", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        history = sharedPreferences.getString("date", "") + "You went to the notes page at: " + currentTime.toString() + "\n";
+        editor.putString("date", history);
+        editor.commit();
     }
 }
