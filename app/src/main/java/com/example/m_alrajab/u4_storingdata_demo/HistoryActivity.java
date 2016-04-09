01eadeabc49792ea0c90b2725638bf86ecc21704
Benjamin <scalerab1@mail.montclair.edu>
2016-04-08 22:33:59 -0400

    I noticed that I forgot to add the history activity so put the history text back to the original size and I commented out where the logActivity method is used because it should have been shown without those changes in the other commits.

---
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/HistoryActivity.java
@@ -0,0 +1,36 @@
+package com.example.m_alrajab.u4_storingdata_demo;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class HistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        //make a record of when the page was accessed
+        //logActivity();
+
+        //show the history that is saved in sharedPreferences
+        TextView history=(TextView)findViewById(R.id.history_viewHistory_textView);
+        SharedPreferences sharedPreferences = getSharedPreferences("history", MODE_PRIVATE);
+        history.setText(sharedPreferences.getString("date", "no history"));
+    }
+
+    public void logActivity()
+    {
+        Date currentTime = new Date();
+        String history;
+        SharedPreferences sharedPreferences = getSharedPreferences("history", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        history = sharedPreferences.getString("date", "") + "You checked your history at: " + currentTime.toString() + "\n";
+        editor.putString("date", history);
+        editor.commit();
+    }
+}
