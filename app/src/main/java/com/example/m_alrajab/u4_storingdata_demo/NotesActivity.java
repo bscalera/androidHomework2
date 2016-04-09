2258b2995258a893c92b1bf2f27270ef97a7b760
Benjamin <scalerab1@mail.montclair.edu>
2016-04-09 03:31:05 -0400

    The space on the device is shown when the view space button is clicked

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/NotesActivity.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/NotesActivity.java
@@ -3,7 +3,11 @@ package com.example.m_alrajab.u4_storingdata_demo;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.Date;
 
 public class NotesActivity extends AppCompatActivity {
@@ -12,9 +16,58 @@ public class NotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-        
+
 
         logActivity();
+
+
+        Button create = (Button) findViewById(R.id.notes_create_button);
+        Button viewNotes = (Button) findViewById(R.id.notes_view_button);
+        Button viewSpace = (Button) findViewById(R.id.notes_freeSpace_button);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        viewNotes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        viewSpace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSpace();
+            }
+        });
+
+
+
+
+    }
+
+    public void showSpace()
+    {
+        File file = new File(NotesActivity.this.getFilesDir().getPath());
+        long totalSpace = file.getTotalSpace();
+        long usableSpace = file.getUsableSpace();
+        long freeSpace = file.getFreeSpace();
+
+        String space = "total space: " + totalSpace + " bytes\n" +
+                "usable space: " + usableSpace + " bytes\n" +
+                "free space: " + freeSpace + " bytes";
+
+        Toast.makeText(NotesActivity.this, space, Toast.LENGTH_LONG).show();
+
+
+
+        //http://blog.evizija.si/android-storage
+        //http://www.mkyong.com/java/how-to-get-free-disk-space-in-java
     }
 
     public void logActivity()
