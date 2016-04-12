4cdeeb6ad5b8ce01476b974a15261f69f85a61f6
Benjamin <scalerab1@mail.montclair.edu>
2016-04-12 03:01:48 -0400

    The history activity is now a scroll view

--- app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/HistoryActivity.java
+++ app/src/main/java/com/example/m_alrajab/u4_storingdata_demo/HistoryActivity.java
@@ -1,8 +1,12 @@
 package com.example.m_alrajab.u4_storingdata_demo;
 
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.TextView;
 
 import java.util.Date;
@@ -13,6 +17,17 @@ public class HistoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         //make a record of when the page was accessed
         logActivity();
@@ -25,6 +40,9 @@ public class HistoryActivity extends AppCompatActivity {
         TextView history=(TextView)findViewById(R.id.history_viewHistory_textView);
         SharedPreferences sharedPreferences = getSharedPreferences(preferences, MODE_PRIVATE);
         history.setText(sharedPreferences.getString("date", "no history"));
+
+
+
     }
 
     public void logActivity()
